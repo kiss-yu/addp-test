a909e67b9d1da021087d94672b787fd372e50fc9
kiss-yu <kiss4400>
2019-05-22 00:16:21 +0800

    shell

--- ADDP-INF/start.sh
+++ ADDP-INF/start.sh
@@ -7,11 +7,15 @@ ENV=$2
 port=$3
 
 # 启动应用镜像
-container="$(docker ps -a |grep ${APP}-${ENV})"
-if [ -z ${container} ]
-    then
-        echo "build container"
-        docker run  --name ${APP}-${ENV}  -p ${port}:80 -v /tmp:/tmp -dit ${APP}:${ENV}
-     else
-        docker start ${APP}-${ENV}
-fi
+#container="$(docker ps -a |grep ${APP}-${ENV})"
+#if [ -z ${container} ]
+#    then
+#        echo "build container"
+#
+#     else
+#        docker start ${APP}-${ENV}
+#fi
+
+docker stop ${APP}-${ENV}
+docker rm ${APP}-${ENV}
+docker run  --name ${APP}-${ENV}  -p ${port}:80 -v /tmp:/tmp -dit ${APP}:${ENV}
