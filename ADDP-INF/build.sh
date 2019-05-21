740010ccf18305a2e2ee25f54ab4a32d1eff55da
kiss-yu <kiss4400>
2019-05-21 22:45:23 +0800

    shell

--- ADDP-INF/build.sh
+++ ADDP-INF/build.sh
@@ -7,18 +7,19 @@ ENV=$2
 port=$3
 # 寻找打包的jar包，移动到当前目录
 jar=$(find ./ -name ${APP}-start*.jar)
-cp ${jar} ./ADDP-INF/${APP}.jar
-build_app_image() {
-    IMAGE="$(docker images|grep ${APP}:${ENV})"
-    if [ -n ${IMAGE} ]
-    then
-        echo "start build base image..."
-        docker build -t ${APP}:${ENV}   --build-arg APP_NAME=${APP} --build-arg ENV=${ENV} -f ./ADDP-INF/Dockerfile ./ADDP-INF
-    else
-        docker rm ${APP}:${ENV}
-    fi
-}
-
-build_app_image
+copy ${jar} ./ADDP-INF/${APP}.jar
+#build_app_image() {
+#    IMAGE="$(docker images|grep ${APP})"
+#    if [ -n ${IMAGE} ]
+#    then
+#        echo "start build base image..."
+#        docker build -t ${APP}:${ENV}   --build-arg APP_NAME=${APP} --build-arg ENV=${ENV} -f ./ADDP-INF/Dockerfile ./ADDP-INF
+#    else
+#        docker rm ${APP}:${ENV}
+#    fi
+#}
+docker rm ${APP}:${ENV}
+docker rmi ${APP}:${ENV}
+docker build -t ${APP}:${ENV}   --build-arg APP_NAME=${APP} --build-arg ENV=${ENV} -f ./ADDP-INF/Dockerfile ./ADDP-INF
 # 启动应用镜像
 docker run  --name ${APP}-${ENV}  -p ${port}:80 -v /tmp:/tmp -dit ${APP}:${ENV}
